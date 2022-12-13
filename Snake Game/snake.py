c77c975d1bb31a58ee55eee1fba5575ab10b9428
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2022-12-13 14:06:11 +0530

    Update snake.py

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -25,6 +25,12 @@ class Snake:
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+        
+        
+    def reset(self):
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
@@ -35,7 +41,8 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
-
+        
+        
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
